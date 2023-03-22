6a38f9a415d066688c47662c2ce96c2f230b9221
ricky <Roll08@outlook.com>
2023-03-22 19:15:11 +0800

    first commmit

---
+++ src/main/java/com/ricky/git/GitTest.java
@@ -0,0 +1,8 @@
+package com.ricky.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello gittt");
+        System.out.println("hello gittt");
+    }
+}

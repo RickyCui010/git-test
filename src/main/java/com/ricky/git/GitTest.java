d7fbf5a15a61f520d5c0282f647d2aed5642305d
ricky <Roll08@outlook.com>
2023-03-22 19:23:20 +0800

    hot-fix commmit1

--- src/main/java/com/ricky/git/GitTest.java
+++ src/main/java/com/ricky/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello gittt");
         System.out.println("hello gittt");
         System.out.println("hello gittt");
+        System.out.println("hello gittt44444");
     }
 }
